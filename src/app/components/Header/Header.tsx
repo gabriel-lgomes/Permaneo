64f4b07b4dc165a9da5868e4d9e3bbe97ba5b93a
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2025-03-28 16:32:23 -0300

    refactor: menu created

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,19 +1,65 @@
+"use client";
+
 import Image from "next/image";
-import React, { FC } from "react";
+import Link from "next/link";
+import React, { FC, useState } from "react";
 
 const Header: FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <header>
       <div className="container mx-auto">
-        <div className="flex gap-4 items-center py-8">
-          <Image
-            src="/grupopermaneo_logo.webp"
-            alt="Grupo Permaneo"
-            width={70}
-            height={70}
-            className="rounded-full"
-          />
-          <h1 className="text-3xl font-black">Grupo Permaneo</h1>
+        <div className="flex items-center justify-between py-8">
+          <div className="flex gap-4 items-center">
+            <Image
+              src="/grupopermaneo_logo.webp"
+              alt="Grupo Permaneo"
+              width={70}
+              height={70}
+              className="rounded-full"
+            />
+            <h1 className="text-3xl font-black">Grupo Permaneo</h1>
+          </div>
+
+          {/* Button to toggle the menu on mobile */}
+          <button className="lg:hidden text-3xl" onClick={toggleMenu}>
+            ☰
+          </button>
+
+          {/* Nav menu */}
+          <div
+            className={`lg:flex ${
+              isMenuOpen
+                ? "block absolute px-2 py-4 bg-secondary max-w-11/12 left-5 top-28 w-full"
+                : "hidden"
+            }  gap-8`}
+          >
+            <nav>
+              <ul className="lg:flex lg:gap-8 transition-all">
+                <li>
+                  <Link
+                    href="/"
+                    className="text-xl hover:text-primary transition-all"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Cursos
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/courses/favorites"
+                    className="text-xl hover:text-primary transition-all"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    Cursos Favoritos
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
