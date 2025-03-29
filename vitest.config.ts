44c212cc9bf0a3f9b6b8694e360aa782d5705c84
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2025-03-28 21:30:04 -0300

    chore: vitest and react test library

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,17 @@
-// vitest.config.ts
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import React from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [React()],
   test: {
-    environment: "jsdom", // Necessário para trabalhar com React
-    globals: true, // Habilita o uso de globals como 'describe', 'it', etc.
-    coverage: {
-      provider: "v8", // Usado para cobertura de testes
-      reporter: ["text", "json", "html"], // Tipos de relatórios de cobertura
-      reportsDirectory: "./coverage", // Onde os relatórios serão gerados
-    },
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/app/setupTests.ts"],
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
 });
